39399ccfed861796beca4de6b97fada82636f9d9
Camilo Tapia <camilo.tapia@gmail.com>
2020-01-14 16:41:57 +0100

    fix new version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 Plugin Name: Draft/Live Sync for Content Service
 Plugin URI: http://24hr.se
 Description: Saves content to a Draft Content Service and gives the possibility to push the content to live
-Version: 0.9.9
+Version: 0.9.10
 Author: Camilo Tapia <camilo.tapia@24hr.se>
 */
 
@@ -17,7 +17,7 @@ if ( !defined( 'ABSPATH' ) ) {
 
     $dir = dirname( __FILE__ );
 
-    define('DraftLiveSyncVERSION', '0.9.9');
+    define('DraftLiveSyncVERSION', '0.9.10');
 
     require_once( $dir . '/lib/draft-live-sync-wp-cron-fix.php' );
     require_once( $dir . '/lib/draft-live-sync-class.php' );
